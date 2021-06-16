64a4d77340f5cc9917d52b821e62c21928d35234
tktkowashi <owashi@toktok.com>
2021-06-16 18:23:30 +0900

    add gacha image

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ function gacha(){
     NFT = Math.random();
 
     if (document.getElementById){
-        document.getElementById("NFTId").textContent = NFT;
+        document.getElementById("gacha-imgfile").src = "img/img-gacha-animation.gif";
+    }
+
+    if (document.getElementById){
+        document.getElementById("NFTId").textContent = "";
+
+        var changeImage = function(){
+            document.getElementById("NFTId").textContent = NFT;
+        };
+        setTimeout(changeImage, 4000);
     }
 }
