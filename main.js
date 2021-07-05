0ee46dce00077371d3292179f265be3b156bd4e4
tktkowashi <owashi@toktok.com>
2021-07-05 15:43:54 +0900

    add metamask

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+// var web3 = new Web3(new Web3.providers.WebsocketProvider("http://192.168.10.24:7545"));
+// const web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.10.24:7545"));
+
+window.addEventListener('load', function() {
+    alert("event listner start");
+    if (typeof web3 !== 'undefined') {
+        web3js = new Web3(web3.currentProvider);
+        alert("succeeded");
+    } else {
+        alert("MetaMaskをインストールしてください");
+    }
+    alert("event listner end");
+});
+
+//console.log(web3js.eth.accounts[0]);
+
+/*
+web3.eth.getAccounts(function(err, accounts) {
+    coinbase = web3.eth.accounts[0];
+    console.log("coinbase is " + coinbase);
+    if (typeof coinbase === 'undefined') {
+        alert("MetaMaskを起動してください")
+    }
+});
+*/
+
 function gacha(){
     NFT = Math.random();
 
@@ -13,4 +39,11 @@ function gacha(){
         };
         setTimeout(changeImage, 4000);
     }
+}
+
+function showETHInfo() {
+    const web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.10.24:7545"));
+    var ETHInfo = web3.eth.accounts[0];
+
+    document.getElementById("ETHInfo").textContent = ETHInfo;
 }
